0df0ac0fff33db7f41e24b96d35d53aa9ba9567a
Francisco Tena <ftenaf@yahoo.es>
2022-05-05 20:17:35 +0100

    added loop for creating more that 100 items with comments and users.

--- backend/seeds/items.js
+++ backend/seeds/items.js
@@ -2,28 +2,39 @@ const mongoose = require('mongoose');
 var Comment = require('../models/Comment');
 var User = require('../models/User');
 var Item = require('../models/Item');
+const { faker } = require('@faker-js/faker');
 
 mongoose.connect(process.env.MONGODB_URI);
-const items = [
-    { 
-        title:"How to train your dragon",
-        description:"Ever wonder how?", 
-        body:"Very carefully.", 
-        tagList:["dragons","training"],
-
-    },
-    { 
-        title:"How to train your dragon2",
-        description:"Ever wonder how again?", 
-        body:"Very Very carefully.", 
-        tagList:["dragons","training"]
+var items = [];
+async function addItems() {    
+    for (var i = 0; i < 200; i++) {
+        let user = new User({
+            username: `UserName${i}`,
+            email: `${i}${faker.internet.email()}`
+        });
+        
+        let item = new Item({
+            title: faker.lorem.words(),
+            description: faker.lorem.lines(),
+            image: faker.image.image(),
+            tagList: faker.hacker.adjective(),
+        });
+        let comment = new Comment({
+            body: faker.lorem.sentences() 
+        })
+        
+        let savedUser = await user.save();
+        item.seller = savedUser;
+        let savedItem = await item.save();
+        
+        comment.seller = savedUser;
+        comment.item = savedItem;                
+        await comment.save();    
+        items.push(item);
     }
-];
-
-const seedDB = async () => {
-    await Item.insertMany(items);
 };
 
-seedDB().then(() => {
+console.log(items.length);
+addItems().then(() => {
     mongoose.connection.close();
-})
+}).then(process.exit);
